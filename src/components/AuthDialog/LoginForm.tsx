df328b2143b26ed328dbe04114c09d68339870ce
ponchik009 <lolioknru@mail.ru>
2022-04-17 23:51:19 +0800

    фиксы + форма логина и регистрации (супервалидация)

--- src/components/AuthDialog/LoginForm.tsx
+++ src/components/AuthDialog/LoginForm.tsx
@@ -1,7 +1,47 @@
+import { Button, DialogActions, DialogContent, TextField } from "@mui/material";
 import React from "react";
 
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+    // запрос на логин, изменение глобал стейта
+  };
+
+  return (
+    <>
+      <DialogContent sx={{ padding: "20px" }}>
+        <TextField
+          label="Email"
+          fullWidth
+          value={email}
+          onChange={handleEmailChange}
+          sx={{ marginTop: "20px" }}
+        />
+        <TextField
+          label="Пароль"
+          fullWidth
+          type="password"
+          sx={{ marginTop: "20px" }}
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button variant="text" onClick={handleLogin}>
+          Войти
+        </Button>
+      </DialogActions>
+    </>
+  );
 };
 
 export default LoginForm;
--- src/components/AuthDialog/RegisterForm.tsx
+++ src/components/AuthDialog/RegisterForm.tsx
@@ -1,7 +1,130 @@
 import React from "react";
+import { Button, DialogActions, DialogContent, TextField } from "@mui/material";
 
 const RegisterForm = () => {
-  return <div>RegisterForm</div>;
+  const [email, setEmail] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [confirmPassword, setConfirmPassword] = React.useState("");
+
+  const [emailError, setEmailError] = React.useState(false);
+  const [nameError, setNameError] = React.useState(false);
+  const [passwordError, setPasswordError] = React.useState(false);
+
+  const [emailErrorText, setEmailErrorText] = React.useState("");
+  const [nameErrorText, setNameErrorText] = React.useState("");
+  const [passwordErrorText, setPasswordErrorText] = React.useState("");
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmailError(false);
+    setEmailErrorText("");
+    setEmail(event.target.value);
+  };
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNameError(false);
+    setNameErrorText("");
+    setName(event.target.value);
+  };
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPasswordError(false);
+    setPasswordErrorText("");
+    setPassword(event.target.value);
+  };
+  const handleConfirmPasswordChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setPasswordError(false);
+    setPasswordErrorText("");
+    setConfirmPassword(event.target.value);
+  };
+
+  const comparePasswords = () => password === confirmPassword;
+
+  const validateEmail = () => {
+    const parts = email.split("@");
+
+    if (parts.length !== 2 || !parts[0].length || !parts[1].length)
+      return false;
+    const domens = parts[1].split(".");
+    if (domens.length !== 2 || !domens[0].length || !domens[1].length)
+      return false;
+
+    return true;
+  };
+
+  const handleEmailError = (text: string) => {
+    setEmailError(true);
+    setEmailErrorText(text);
+  };
+  const handleNameError = (text: string) => {
+    setNameError(true);
+    setNameErrorText(text);
+  };
+  const handlePasswordError = (text: string) => {
+    setPasswordError(true);
+    setPasswordErrorText(text);
+  };
+
+  const validate = () => {
+    if (!email.length) handleEmailError("Заполните поле!");
+    else if (!validateEmail()) handleEmailError("Невалидный email!");
+    if (!name.length) handleNameError("Заполните поле!");
+    if (!password.length) handlePasswordError("Заполните поле!");
+    else if (!comparePasswords()) handlePasswordError("Пароли не совпадают!");
+  };
+
+  const handleRegister = () => {
+    validate();
+    // запрос на регистрацию, изменение глобал стейта
+  };
+
+  return (
+    <>
+      <DialogContent sx={{ padding: "20px" }}>
+        <TextField
+          label="Email"
+          fullWidth
+          value={email}
+          onChange={handleEmailChange}
+          sx={{ marginTop: "20px" }}
+          error={emailError}
+          helperText={emailErrorText}
+        />
+        <TextField
+          label="Имя"
+          fullWidth
+          value={name}
+          onChange={handleNameChange}
+          sx={{ marginTop: "20px" }}
+          error={nameError}
+          helperText={nameErrorText}
+        />
+        <TextField
+          label="Пароль"
+          fullWidth
+          type="password"
+          sx={{ marginTop: "20px" }}
+          value={password}
+          onChange={handlePasswordChange}
+          error={passwordError}
+          helperText={passwordErrorText}
+        />
+        <TextField
+          label="Подтверждение пароля"
+          fullWidth
+          type="password"
+          sx={{ marginTop: "20px" }}
+          value={confirmPassword}
+          onChange={handleConfirmPasswordChange}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button variant="text" onClick={handleRegister}>
+          Зарегистрироваться
+        </Button>
+      </DialogActions>
+    </>
+  );
 };
 
 export default RegisterForm;
