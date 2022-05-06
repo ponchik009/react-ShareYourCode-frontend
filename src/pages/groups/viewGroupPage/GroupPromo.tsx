38ae1555009d3b7fd4e70713e0b97b4efb1e3653
ponchik009 <lolioknru@mail.ru>
2022-05-06 19:47:06 +0800

    страница описания группы

--- src/pages/groups/viewGroupPage/GroupPromo.tsx
+++ src/pages/groups/viewGroupPage/GroupPromo.tsx
@@ -8,12 +8,25 @@ interface IGroupPromoProps {
 }
 
 const GroupPromo: React.FC<IGroupPromoProps> = ({ handleEnter, group }) => {
+  console.log(group);
   return (
     <>
-      <Box>
-        <Typography>{group.name}</Typography>
-        <Typography>{group.description}</Typography>
-        <Button onClick={handleEnter}>Вступить</Button>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "start",
+          justifyContent: "space-between",
+          flexDirection: "column",
+        }}
+      >
+        <Typography variant="h4">Просмотр информации о группе</Typography>
+        <Typography>Название группы: {group.name}</Typography>
+        <Typography>Описание: {group.description}</Typography>
+        <Typography>Количество участников: {group.members.length}</Typography>
+        <Typography>Количество тредов: {group.treds.length}</Typography>
+        <Box sx={{ margin: "auto" }}>
+          <Button onClick={handleEnter}>Вступить</Button>
+        </Box>
       </Box>
     </>
   );
