8bfb646b62d81191d37337a5ad3903212ffcbe11
ponchik009 <lolioknru@mail.ru>
2022-05-08 12:09:11 +0800

    кол-во участников в списке сообществ

--- src/pages/groups/groupsPage/GroupsList/GroupItem.tsx
+++ src/pages/groups/groupsPage/GroupsList/GroupItem.tsx
@@ -11,8 +11,12 @@ interface IProps {
 const GroupItem: React.FC<IProps> = ({ group, onClick }) => {
   return (
     <>
-      <ListItemButton onClick={onClick}>
+      <ListItemButton
+        onClick={onClick}
+        sx={{ display: "flex", justifyContent: "space-between" }}
+      >
         <Typography>{group.name}</Typography>
+        <Typography>Участников: {group.membersCount}</Typography>
       </ListItemButton>
       <Divider />
     </>
